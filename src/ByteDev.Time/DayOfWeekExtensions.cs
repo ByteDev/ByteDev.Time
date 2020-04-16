b7619ec6e71a538ea2ae6b2b9fa1e73929f520c9
Paul Stanley <paulstanley@protonmail.com>
2020-04-16 18:41:45 +0800

    Added DayOfWeekExtensions

---
+++ src/ByteDev.Time/DayOfWeekExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ByteDev.Time
+{
+    /// <summary>
+    /// Extension methods for <see cref="T:System.DayOfWeek" />.
+    /// </summary>
+    public static class DayOfWeekExtensions
+    {
+        /// <summary>
+        /// Returns a three character representation of the day of the week.
+        /// </summary>
+        /// <param name="source">The day of the week.</param>
+        /// <returns>Short representation of the day of the week.</returns>
+        /// <exception cref="T:System.ArgumentException"><paramref name="source" />Value is not defined.</exception>
+        public static string ToStringShort(this DayOfWeek source)
+        {
+            if (!Enum.IsDefined(typeof(DayOfWeek), source))
+            {
+                throw new ArgumentException($"No {typeof(DayOfWeek)} defined for value: '{source}'.", nameof(source));
+            }
+
+            return source.ToString().Substring(0, 3);
+        }
+    }
+}
---
+++ tests/ByteDev.Time.UnitTests/DayOfWeekExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ByteDev.Time.UnitTests
+{
+    [TestFixture]
+    public class DayOfWeekExtensionsTests
+    {
+        [TestFixture]
+        public class ToStringShort
+        {
+            [TestCase(DayOfWeek.Monday, "Mon")]
+            [TestCase(DayOfWeek.Tuesday, "Tue")]
+            [TestCase(DayOfWeek.Wednesday, "Wed")]
+            [TestCase(DayOfWeek.Thursday, "Thu")]
+            [TestCase(DayOfWeek.Friday, "Fri")]
+            [TestCase(DayOfWeek.Saturday, "Sat")]
+            [TestCase(DayOfWeek.Sunday, "Sun")]
+            public void WhenIsDefined_ThenReturnCorrectString(DayOfWeek dayOfWeek, string expected)
+            {
+                var result = dayOfWeek.ToStringShort();
+
+                Assert.That(result, Is.EqualTo(expected));
+            }
+
+            [Test]
+            public void WhenIsNotDefined_ThenThrowException()
+            {
+                Assert.Throws<ArgumentException>(() => ((DayOfWeek)8).ToStringShort());
+            }
+        }
+    }
+}
