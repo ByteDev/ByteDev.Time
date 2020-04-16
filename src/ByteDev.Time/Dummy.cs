88be7912426e1870545be1739b84bb76e9e793a2
Paul Stanley <paulstanley@protonmail.com>
2020-04-16 17:58:10 +0800

    Initial commit

---
+++ src/ByteDev.Time/Dummy.cs
@@ -0,0 +1,17 @@
+﻿namespace ByteDev.Template.LibraryPackage
+{
+    /// <summary>
+    /// Dummy class.
+    /// </summary>
+    public class Dummy
+    {
+        /// <summary>
+        /// Execute and return true.
+        /// </summary>
+        /// <returns>True.</returns>
+        public bool Execute()
+        {
+            return true;
+        }
+    }
+}
